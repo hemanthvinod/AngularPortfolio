dff92cbd5c91ed7f7e3017bdd8c527e09e684cec
Hemanth <vinodhemanth@gmail.com>
2022-07-19 00:14:18 +0530

    third

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+// const express = require("express");
+// const path = require("path");
+// const app = express();
 
-app.use(express.static(__dirname + "/dist/project"));
+// app.use(express.static(__dirname + "/dist/project"));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname + "dist/project/index.html"));
-});
+// app.get("/*", (req, res) => {
+//   res.sendFile(path.join(__dirname + "dist/project/index.html"));
+// });
 
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
